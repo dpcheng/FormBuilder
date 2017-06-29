26c95f990326d965a7489e6f06c9fcd01c709ac2
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-28 23:46:43 -0700

    add state to Index, can click on tabs to change state and styling

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -3,15 +3,27 @@ import React from 'react';
 class Index extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {tab: "Create"};
+    this.selectTab = this.selectTab.bind(this);
+  }
+
+  deselectTab() {
+    const currentTab = document.getElementById(this.state.tab);
+    currentTab.className = "";
+  }
+
+  selectTab(e) {
+    this.deselectTab();
+    e.currentTarget.className = "selected";
+    this.setState({ tab: e.currentTarget.textContent });
   }
 
   render() {
     return (
       <nav>
-        <li>Create</li>
-        <li>Preview</li>
-        <li>Export</li>
+        <li id="Create" className="selected" onClick={ this.selectTab }>Create</li>
+        <li id="Preview" onClick={ this.selectTab }>Preview</li>
+        <li id="Export" onClick={ this.selectTab }>Export</li>
       </nav>
     );
   }
