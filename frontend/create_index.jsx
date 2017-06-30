aac1e0ccefb5fae282151ffbea9e18f3b4cac597
Dylan <dylan.patrick.cheng@gmail.com>
2017-06-29 19:44:04 -0700

    create addInput method, adds to localStorage. read from localStorage when mounting CreateIndex

--- frontend/create_index.jsx
+++ frontend/create_index.jsx
@@ -3,12 +3,31 @@ import React from 'react';
 class CreateIndex extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {form: {}};
+  }
+
+  componentWillMount() {
+    const prevForm = JSON.parse(localStorage.getItem('form'));
+    if (prevForm) {
+      this.setState({ form: prevForm });
+    }
+  }
+
+  addInput() {
+    const inputCount = Object.keys(this.state.form).length;
+    let form = this.state.form;
+    form[inputCount] = {};
+    this.setState({ form });
+    localStorage.setItem('form', JSON.stringify(this.state.form));
   }
 
   render() {
     return (
-      <main>Create Content</main>
+      <main>
+        Create Content
+        <li>{JSON.stringify(this.state.form)}</li>
+        <button onClick={ this.addInput.bind(this) }>Add Input</button>
+      </main>
     );
   }
 }
